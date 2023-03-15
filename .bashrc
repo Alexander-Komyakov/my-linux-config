6863395622dd8b5da4959b2cd8953f090a8ced1c
Alexander-Komyakov <sanya202018@gmail.com>
2023-03-16 02:13:45 +0300

    fix conf installer

--- .bashrc
+++ .bashrc
@@ -10,4 +10,3 @@ alias ls='ls --color=auto'
 alias ll='ls --color=auto -al'
 alias gosw='cd ~/.local/share/StartWine/data/scripts'
 PS1='\[\e[33m\]\u\[\e[31m\]@\[\e[32m\]\h:\W\[\e[37m\]$ '
-gitHub="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"
--- conf_installer.sh
+++ conf_installer.sh
@@ -3,34 +3,36 @@
 
 dp_env=$1
 path_to_file="${0%/*}"
-
-cp $path_to_file/.vimrc ~/.vimrc
-cp $path_to_file/.bashrc ~/.bashrc
-cp $path_to_file/.xinitrc ~/.xinitrc
-cp $path_to_file/.Xmodmap ~/.Xmodmap
-cp $path_to_file/config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
+mkdir -p ~/.config/xfce4/terminal/
+cp "$path_to_file/.vimrc" ~/.vimrc
+cp "$path_to_file/.bashrc" ~/.bashrc
+cp "$path_to_file/.xinitrc" ~/.xinitrc
+cp "$path_to_file/.Xmodmap" ~/.Xmodmap
+cp "$path_to_file/config/xfce4/terminal/terminalrc" ~/.config/xfce4/terminal/terminalrc
 
 sudo pacman -Sy
-if [[! -z pacman -Qs | grep yay ]]; then
+
+yayb=`pacman -Qs | grep yay`
+if [ -z "$yayb" ]; then
 	git clone https://aur.archlinux.org/yay-git.git && cd yay-git
 	exec makepkg -si --noconfirm
 	cd .. && rm -rf yay-git
 fi
 
-sudo pacman -S imagemagick i3lock picom\
+sudo pacman -S imagemagick i3lock picom xorg\
 	xorg-setxkbmap vim xfce4-terminal git parcellite --noconfirm
 
-if [[dp_env == "qtile"]]; then
+if [[ "$dp_env" == "qtile" ]]; then
 	sudo pacman -S qtile --noconfirm
-	cp $path_to_file/config/qtile/config.py ~/.config/qtile/config.py
-	cp $path_to_file/config/qtile/autostart.sh ~/.config/qtile/autostart.sh
-	cp $path_to_file/config/qtile/keyboard.sh ~/.config/qtile/keyboard.sh
-elif [[dp_env == "i3"]]; then
-	sudo pacman -S i3gaps polybar --noconfirm
+	cp "$path_to_file"/config/qtile/config.py ~/.config/qtile/
+	cp "$path_to_file"/config/qtile/autostart.sh ~/.config/qtile/
+	cp "$path_to_file"/config/qtile/keyboard.sh ~/.config/qtile/
+elif [[ "$dp_env" == "i3" ]]; then
+	sudo pacman -S dmenu i3-gaps feh polybar --noconfirm
 	yay -S i3lock-multimonitor --noconfirm
-	$path_to_file/config/i3/config ~/.config/i3/config
-	$path_to_file/config/polybar/config.ini ~/.config/i3/config.ini
-	$path_to_file/config/polybar/launch.ini ~/.config/i3/launch.ini
+	cp "$path_to_file"/config/i3/config ~/.config/i3/
+	cp "$path_to_file"/config/polybar/config.ini ~/.config/polybar/
+	cp "$path_to_file"/config/polybar/launch.ini ~/.config/polybar/
 fi
 
 
